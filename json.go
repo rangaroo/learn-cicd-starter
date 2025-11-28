03f0a1481e803330ee33a30fd86c4b624c09c3e9
Aslan Onalbek <aslan.onalbek@nu.edu.kz>
2025-11-29 03:07:02 +0500

    Fix vulnerabilities

--- json.go
+++ json.go
@@ -30,5 +30,11 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Error calling w.Write(dat): %s", err)
+		w.WriteHeader(500)
+		return
+	}
 }
